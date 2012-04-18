d3c25bda520911caf1868bf11dcfb978feaa83fa
Josh Perez <josh@goatslacker.com>
2012-04-17 22:27:13 -0700

    YAY all compiles

--- esprima.js
+++ esprima.js
@@ -1,6 +1,6 @@
 var util = require('util');
 var esprima = require('esprima');
 var escodegen = require('escodegen');
-var ast = esprima.parse('var lastEl = function lastEl(a) { return a }');
+var ast = esprima.parse('(function () { return print("hi") }())');
 //console.log(escodegen.generate(ast));
 util.puts(util.inspect(ast, false, 10));
