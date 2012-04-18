cbbf59bdd6f3d7f90a1d017e23483d3be6c74ae2
Josh Perez <josh@goatslacker.com>
2012-04-17 21:12:58 -0700

    more hacks

--- esprima.js
+++ esprima.js
@@ -1,7 +1,6 @@
 var util = require('util');
 var esprima = require('esprima');
 var escodegen = require('escodegen');
-var ast = esprima.parse('(function () { return print(1) }())');
-console.log(ast);
-console.log(escodegen.generate(ast));
-//util.puts(util.inspect(ast, false, 10));
+var ast = esprima.parse('head([1, 2, 3])');
+//console.log(escodegen.generate(ast));
+util.puts(util.inspect(ast, false, 10));
