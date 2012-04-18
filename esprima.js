dd87947b03d595696a70def151ee8b710fd80668
Josh Perez <josh@goatslacker.com>
2012-04-17 19:57:00 -0700

    lists

--- esprima.js
+++ esprima.js
@@ -1,4 +1,4 @@
 var util = require('util');
 var esprima = require('esprima');
-var ast = esprima.parse('var foo = function foo(n) { return n; };');
+var ast = esprima.parse('var foo = function foo(n) { return [n]; };');
 util.puts(util.inspect(ast.body[0], false, 10));
