ce6d619d5f285cee5f70d9580afb88d300d885f7
Josh Perez <josh@goatslacker.com>
2012-05-23 21:26:53 -0700

    declares var as a single var

--- esprima.js
+++ esprima.js
@@ -1,6 +1,6 @@
 var util = require('util');
 var esprima = require('esprima');
 var escodegen = require('escodegen');
-var ast = esprima.parse('print(2);\nfunction head(list) { return list[0] }');
+var ast = esprima.parse('var a, b');
 //console.log(escodegen.generate(ast));
 util.puts(util.inspect(ast, false, 14));
