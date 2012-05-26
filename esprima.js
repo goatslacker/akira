a530696bbbb2bd36adb6995a2a899f94ee190b22
Josh Perez <josh@goatslacker.com>
2012-05-26 14:23:56 -0700

    Adds exceptions

--- esprima.js
+++ esprima.js
@@ -1,6 +1,6 @@
 var util = require('util');
 var esprima = require('esprima');
 var escodegen = require('escodegen');
-var ast = esprima.parse('var a, b');
+var ast = esprima.parse('try { var foo; } catch (e) { var foo; }');
 //console.log(escodegen.generate(ast));
 util.puts(util.inspect(ast, false, 14));
